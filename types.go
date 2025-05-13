f6ddacf645019f84d2f501aadec835ba8ef68968
evanson <evansonmwangi83@gmail.com>
2025-05-13 15:47:09 +0300

    shared type information

---
+++ types.go
@@ -0,0 +1,34 @@
+package kai
+
+// DeploymentParams holds all possible deployment configuration parameters
+type DeploymentParams struct {
+	Name             string
+	Namespace        string
+	Image            string
+	Replicas         float64
+	Labels           map[string]interface{}
+	ContainerPort    string
+	Env              map[string]interface{}
+	ImagePullPolicy  string
+	ImagePullSecrets []interface{}
+}
+
+// PodParams holds all possible pod configuration parameters
+type PodParams struct {
+	Name               string
+	Namespace          string
+	Image              string
+	Command            []string
+	Args               []string
+	Labels             map[string]interface{}
+	ContainerName      string
+	ContainerPort      string
+	Env                map[string]interface{}
+	ImagePullPolicy    string
+	ImagePullSecrets   []interface{}
+	RestartPolicy      string
+	NodeSelector       map[string]interface{}
+	ServiceAccountName string
+	Volumes            []interface{}
+	VolumeMounts       []interface{}
+}
