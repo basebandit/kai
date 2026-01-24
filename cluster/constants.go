46faaedd9cddff821a59da776f4eafacdc597190
evanson <evansonmwangi83@gmail.com>
2026-01-24 10:23:52 +0300

    chore: remove unused test constants

--- cluster/constants.go
+++ cluster/constants.go
@@ -10,32 +10,13 @@ const (
 	emptyNamespace   = "empty-namespace"
 
 	// Pod constants
-	podName            = "test-pod"
-	pod1Name           = "pod1"
-	pod2Name           = "pod2"
-	pod3Name           = "pod3"
-	fullPodName        = "full-pod"
-	noImagePodName     = "no-image-pod"
-	forcePodName       = "force-pod"
-	pendingPodName     = "pending-pod"
 	nonexistentPodName = "nonexistent-pod"
 
 	// Container constants
-	containerName        = "test-container"
-	customContainer      = "custom-container"
 	nonexistentContainer = "nonexistent-container"
 
 	// Image constants
 	nginxImage = "nginx:latest"
-
-	// Policy constants
-	alwaysPullPolicy = "Always"
-	onFailurePolicy  = "OnFailure"
-	neverPolicy      = "Never"
-
-	// Service account and secrets
-	testServiceAccount = "test-sa"
-	registrySecret     = "registry-secret"
 
 	// Test cluster constant
 	testCluster  = "test-cluster"
@@ -56,12 +37,6 @@ const (
 	testUser  = "test-user"
 	testUser1 = "user1"
 	testUser2 = "user2"
-
-	// Message templates
-	podCreatedFmt    = "Pod %q created successfully"
-	notFoundErrMsg   = "not found"
-	deleteSuccessMsg = "Successfully delete pod"
-	noPodsFoundMsg   = "no pods found"
 
 	// Deployment constants
 	deploymentName1 = "deployment1"
